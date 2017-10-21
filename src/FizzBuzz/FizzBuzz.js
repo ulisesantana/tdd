ca4709b5e8b5782499cf65ff4a7784cf89aa2f7f
Ulises Santana <u.santana@kentech-sp.com>
2017-10-21 12:15:06 +0100

    GREEN: No deben ser Fizz

--- src/FizzBuzz/FizzBuzz.js
+++ src/FizzBuzz/FizzBuzz.js
@@ -1,13 +1,13 @@
 const StringWrapper = require('./StringWrapper');
 
-class FizzBuzz{
-  constructor(){
-    
+class FizzBuzz {
+  constructor() {
+
   }
 
-  validate(integer){
-    if(integer.number===3) return (new StringWrapper("Fizz"));
-    if(integer.number===5) return (new StringWrapper("Buzz"));
+  validate(integer) {
+    if (integer.number % 3 === 0) return (new StringWrapper("Fizz"));
+    if (integer.number === 5) return (new StringWrapper("Buzz"));
     return integer;
   }
 
--- src/FizzBuzz/FizzBuzz.test.js
+++ src/FizzBuzz/FizzBuzz.test.js
@@ -3,32 +3,56 @@ const Integer = require('./Integer');
 const StringWrapper = require('./StringWrapper');
 const assert = require('assert');
 
-describe('Tests para el FizzBuzz', function(){
-  
+describe('Tests para el FizzBuzz', function () {
+
   const fizz = new StringWrapper("Fizz");
   const buzz = new StringWrapper("Buzz");
-  
-  it('Debe ser un true', function(){
+
+  it('Debe ser un true', function () {
     assert.equal(true, true);
   });
 
-  it('Debe ser un Integer(1)', function(){
+  it('Debe ser un Integer(1)', function () {
     let one = new Integer(1);
     let fb = new FizzBuzz();
     assert.equal(fb.validate(one), one);
   });
 
-  it('Debe ser un Fizz', function(){
-    let three = new Integer(3);
-    let fb = new FizzBuzz();
-    assert.deepEqual(fb.validate(three), fizz);
+  describe("Deben ser Fizz", function () {
+    const useCases = [3, 6, 9]
+
+    useCases.forEach((useCase) => {
+
+      it(`Caso de uso: ${useCase}`, function () {
+        let integer = new Integer(useCase);
+        let fb = new FizzBuzz();
+        assert.deepEqual(fb.validate(integer), fizz);
+      });
+    });
   });
 
-  it('Debe ser un Buzz', function(){
-    let five = new Integer(5);
-    let fb = new FizzBuzz();
-    assert.deepEqual(fb.validate(five), buzz);
+  describe("No deben ser Fizz", function () {
+    const useCases = [2, 4, 5];
+
+    useCases.forEach((useCase) => {
+
+      it(`Caso de uso: ${useCase}`, function () {
+        let integer = new Integer(useCase);
+        let fb = new FizzBuzz();
+        assert.notDeepEqual(fb.validate(integer), fizz);
+      });
+    });
   });
-  
-  
+
+
+  describe("Deben ser Buzz", function () {
+    it('Buzz para 5', function () {
+      let five = new Integer(5);
+      let fb = new FizzBuzz();
+      assert.deepEqual(fb.validate(five), buzz);
+    });
+  });
+
+
+
 });
