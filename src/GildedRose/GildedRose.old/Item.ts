7e3f0b55840c856f2e48615c44228e3deb11b1b8
Ulises Santana <ulisesantana@gmail.com>
2017-10-28 20:30:15 +0100

    Update Item.old.ts

--- src/GildedRose/GildedRose.old/Item.ts
+++ src/GildedRose/GildedRose.old/Item.ts
@@ -1,13 +1,9 @@
-import itemUpdater from './ItemUpdater';
 
 export default function Item(name: string, sellIn: number, quality: number) {
   return {
     name: name,
     sellIn: sellIn,
     quality: quality,
-    passDay: () => {
-      itemUpdater(this);
-    },
     print: function print() {
       return name + ", " + sellIn + ", " + quality;
     }
